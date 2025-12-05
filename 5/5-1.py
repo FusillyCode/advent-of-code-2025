a8a6d17834db636232a33a4ac88014e5aaa8d216
FusillyCode <173061031+FusillyCode@users.noreply.github.com>
2025-12-05 16:14:09 +0100

    5-1 faster method

--- 5/5-1.py
+++ 5/5-1.py
@@ -1,18 +1,22 @@
 from os import chdir, path
 
+def is_fresh(ingredient_id: int, fresh_ranges: list[tuple[int, int]]) -> bool:
+    for start, end in fresh_ranges:
+        if start <= ingredient_id <= end:
+            return True
+    return False
+
 def main():
     chdir(path.realpath(path.dirname(__file__)))
-    fresh_ids = set()
-    available_ids = set()
-    with open('./test.txt', 'r') as f:
+    fresh_ranges = []
+    with open('./input.txt', 'r') as f:
         while (line := f.readline()) != "\n": # Read lines until finding an empty one
             start, stop = line.split("-")
-            fresh_ids.update(range(int(start), int(stop)+1))
-
-        while (line := f.readline()) != "": # Read lines until EOF
-            available_ids.add(int(line.strip("\n")))
-    fresh_and_available = fresh_ids.intersection(available_ids)
-    print(len(fresh_and_available))
+            fresh_ranges.append((int(start), int(stop)))
+        count = 0
+        for ingredient_id in f.readlines():
+            count += is_fresh(int(ingredient_id), fresh_ranges)
+    print(count)
 
 if __name__ == "__main__":
     main()
